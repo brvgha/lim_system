b958c1958e2f75f775570507b5fe4e42c629413b
brvgha <brughafitzpatrick@gmail.com>
2022-11-23 22:19:58 +0000

    Update register.py

--- register.py
+++ register.py
@@ -15,6 +15,7 @@ def register():
     except:
         pass
 
+    # takes the new account details from the user
     user = input('Please enter your username: ')
     forename = input('Please enter your forename: ')
     surname = input('Please enter your surname: ')
@@ -24,20 +25,22 @@ def register():
 
     password, char_lst, encrypt_lst = encrypt_pass(password)
 
+    # checks last entry into table for their ID and adds 1
     cursor.execute('SELECT id FROM users;')
     result = cursor.fetchall()
     last = result[-1]
     res = int(''.join(map(str, last)))
     res += 1
 
+    # this checks to make sure the user is not already in the database
     cursor.execute('SELECT user FROM users;')
     table = cursor.fetchall()
-    check = None
+    check = False
     for i in range(len(table)):
         if user == table[i][0]:
             check = True
 
-    if check == None:
+    if not check:
         cursor.execute('INSERT INTO users VALUES(?,?,?,?,?);',
                        (res, user, forename, surname, password))
         connection.commit()
